139001b120002842a4720b987b0aac13f47481c3
ー 灰桜 ー <hhbilly99@gmail.com>
2020-01-26 14:45:15 +0800

    modified vars's name

--- src/views/PowMon.vue
+++ src/views/PowMon.vue
@@ -11,38 +11,38 @@
             <el-row :gutter="2">
               <el-col :span="4">
                 <span class="item-title">变压器A相</span>
-                <p class="item-value">{{ PowMonData.transA }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="PowMonData.transA > 80"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.trasApha }}&nbsp;<small>&#176;C</small></p>
+                <p v-if="$root.devStatus.trasApha > 80"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">变压器B相</span>
-                <p class="item-value">{{ PowMonData.transB }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="PowMonData.transB > 80"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.trasBpha }}&nbsp;<small>&#176;C</small></p>
+                <p v-if="$root.devStatus.trasBpha > 80"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">变压器C相</span>
-                <p class="item-value">{{ PowMonData.transC }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="PowMonData.transC > 80"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.trasCpha }}&nbsp;<small>&#176;C</small></p>
+                <p v-if="$root.devStatus.trasCpha > 80"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">母排A相</span>
-                <p class="item-value">{{ PowMonData.busA }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="PowMonData.busA > 80"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.bbarAtem }}&nbsp;<small>&#176;C</small></p>
+                <p v-if="$root.devStatus.bbarAtem > 80"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">母排B相</span>
-                <p class="item-value">{{ PowMonData.busB }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="PowMonData.busB > 80"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.bbarBtem }}&nbsp;<small>&#176;C</small></p>
+                <p v-if="$root.devStatus.bbarBtem > 80"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">母排C相</span>
-                <p class="item-value">{{ PowMonData.busC }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="PowMonData.busC > 80"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.bbarCtem }}&nbsp;<small>&#176;C</small></p>
+                <p v-if="$root.devStatus.bbarCtem > 80"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
             </el-row>
@@ -107,19 +107,19 @@
             <el-row :gutter="2">
               <el-col :span="6">
                 <span class="item-title">电压</span>
-                <p class="item-value">{{ PowMonData.upsV }}&nbsp;<small>V</small></p>
+                <p class="item-value">{{ $root.devStatus.upsoVolt }}&nbsp;<small>V</small></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">电流</span>
-                <p class="item-value">{{ PowMonData.upsI }}&nbsp;<small>A</small></p>
+                <p class="item-value">{{ $root.devStatus.upsoCurr }}&nbsp;<small>A</small></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">频率</span>
-                <p class="item-value">{{ PowMonData.upsF }}&nbsp;<small>Hz</small></p>
+                <p class="item-value">{{ $root.devStatus.upsoFreq }}&nbsp;<small>Hz</small></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">功率</span>
-                <p class="item-value">{{ PowMonData.upsP }}&nbsp;<small>KW</small></p>
+                <p class="item-value">{{ $root.devStatus.upsoPowe }}&nbsp;<small>KW</small></p>
               </el-col>
             </el-row>
           </div>
@@ -136,38 +136,38 @@
             <el-row :gutter="2">
               <el-col :span="4">
                 <span class="item-title">电流</span>
-                <p class="item-value">{{ PowMonData.transI }}&nbsp;<small>A</small></p>
-                <p v-if="PowMonData.transI > 800"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.tratCurr }}&nbsp;<small>A</small></p>
+                <p v-if="$root.devStatus.tratCurr > 800"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">电压</span>
-                <p class="item-value">{{ PowMonData.transV }}&nbsp;<small>V</small></p>
-                <p v-if="PowMonData.transV > 400"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.traoVolt }}&nbsp;<small>V</small></p>
+                <p v-if="$root.devStatus.traoVolt > 400"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">频率</span>
-                <p class="item-value">{{ PowMonData.transF }}&nbsp;<small>Hz</small></p>
-                <p v-if="PowMonData.transF > 50"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.traoFreq }}&nbsp;<small>Hz</small></p>
+                <p v-if="$root.devStatus.traoFreq > 50"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">功率因数</span>
-                <p class="item-value">{{ PowMonData.powF }}</p>
-                <p v-if="PowMonData.powF > 0.5"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.traoPfcc }}</p>
+                <p v-if="$root.devStatus.traoPfcc > 0.5"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">有功电度</span>
-                <p class="item-value">{{ PowMonData.actE }}&nbsp;<small>KWH</small></p>
-                <p v-if="PowMonData.actE > 1000"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.trasPwhb }}&nbsp;<small>KWH</small></p>
+                <p v-if="$root.devStatus.trasPwhb > 1000"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">剩余电流</span>
-                <p class="item-value">{{ PowMonData.resI }}&nbsp;<small>mA</small></p>
-                <p v-if="PowMonData.resI > 0.1"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.resiCurr }}&nbsp;<small>mA</small></p>
+                <p v-if="$root.devStatus.resiCurr > 0.1"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
             </el-row>
@@ -184,14 +184,14 @@
             <el-row :gutter="2">
               <el-col :span="6">
                 <span class="item-title">泄漏电流</span>
-                <p class="item-value">{{ PowMonData.leakI }}&nbsp;<small>mA</small></p>
-                <p v-if="PowMonData.leakI > 1"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.leakCurr }}&nbsp;<small>mA</small></p>
+                <p v-if="$root.devStatus.leakCurr > 1"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">雷击次数</span>
-                <p class="item-value">{{ PowMonData.stkNum }}&nbsp;<small>PCS</small></p>
-                <p v-if="PowMonData.stkNum > 10"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.thunStri }}&nbsp;<small>PCS</small></p>
+                <p v-if="$root.devStatus.thunStri > 10"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="6">
@@ -220,19 +220,19 @@
             <el-row :gutter="2">
               <el-col :span="6">
                 <span class="item-title">负荷率</span>
-                <p class="item-value">{{ PowMonData.upsLd }}&nbsp;<small>&#37;</small></p>
+                <p class="item-value">{{ $root.devStatus.upsoLoad }}&nbsp;<small>&#37;</small></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">电池温度</span>
-                <p class="item-value">{{ PowMonData.batT }}&nbsp;<small>&#176;C</small></p>
+                <p class="item-value">{{ $root.devStatus.upsbTemp }}&nbsp;<small>&#176;C</small></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">电池电压</span>
-                <p class="item-value">{{ PowMonData.batV }}&nbsp;<small>V</small></p>
+                <p class="item-value">{{ $root.devStatus.upsbVolt }}&nbsp;<small>V</small></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">故障码</span>
-                <p class="item-value" style="color:red;">{{ PowMonData.error }}</p>
+                <p class="item-value" style="color:red;">{{ $root.devStatus.upsfCode }}</p>
               </el-col>
             </el-row>
           </div>
@@ -244,36 +244,15 @@
 </template>
 
 <script>
+// import GLOBAL from '@/api/global_variable'
+
 export default {
   name: 'PowMon',
   components: {
   },
   data() {
     return {
-      PowMonData: {
-        transA: '57.2',
-        transB: '56.9',
-        transC: '58.4',
-        busA: '48.3',
-        busB: '45.2',
-        busC: '44.6',
-        upsV: '387.1',
-        upsI: '755.2',
-        upsF: '49.9',
-        upsP: '0.8',
-        transI: '755.2',
-        transV: '387.1',
-        transF: '49.9',
-        powF: '0.82',
-        actE: '994.3',
-        resI: '0.6',
-        leakI: '0.02',
-        stkNum: '1',
-        upsLd: '47.2',
-        batT: '37.8',
-        batV: '73.6',
-        error: '002',
-      },
+      // DevStatus: GLOBAL.DevStatus,
       PowMonStatus: {
         ats: true,
         hp: false,
