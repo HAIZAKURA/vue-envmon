c25d95a636d61f039464db372a0542412345370f
ー 灰桜 ー <hhbilly99@gmail.com>
2020-01-26 14:41:56 +0800

    add function to get device details

--- src/main.js
+++ src/main.js
@@ -10,7 +10,57 @@ Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$axios = axios;
 
+Vue.prototype.$createGetDevStatus = function() {
+  if (this.getTimer) {
+    clearInterval(this.getTimer)
+    this.getTimer = setInterval(() => {
+      this.$axios.get('dev/GetStatus').then(res => (this.$root.devStatus = res.data))
+    }, 2000)
+  } else {
+    this.getTimer = setInterval(() => {
+      this.$axios.get('dev/GetStatus').then(res => (this.$root.devStatus = res.data))
+    }, 2000)
+  }
+}
+
+Vue.prototype.$destoryGetDevStatus = function() {
+  clearInterval(this.getTimer)
+}
+
+Vue.prototype.$createGet = function() {
+  if (this.getTimer) {
+    clearInterval(this.getTimer)
+    this.getTimer = setInterval(() => {
+      this.$axios.get('dev/GetStatus').then(resStatus => (this.$root.devStatus = resStatus.data))
+      // this.$axios.get('dev/GetCfg').then(resCfg => (this.$root.devStatus = resCfg.data))
+      setTimeout(() => {
+        this.$axios.get('dev/GetCfg').then(resCfg => (this.$root.devCfg = resCfg.data))
+      }, 750)
+    }, 1500)
+  } else {
+    this.getTimer = setInterval(() => {
+      this.$axios.get('dev/GetStatus').then(resStatus => (this.$root.devStatus = resStatus.data))
+      // this.$axios.get('dev/GetCfg').then(resCfg => (this.$root.devStatus = resCfg.data))
+      setTimeout(() => {
+        this.$axios.get('dev/GetCfg').then(resCfg => (this.$root.devCfg = resCfg.data))
+      }, 750)
+    }, 1500)
+  }
+}
+
+Vue.prototype.$destoryGet = function() {
+  clearInterval(this.getTimer)
+}
+
 new Vue({
+  data: function() {
+    return {
+      saveInterval: 1,
+      getStatus: 1,
+      devStatus: {},
+      devCfg: {}
+    }
+  },
   router,
   store,
   render: function (h) { return h(App) }
