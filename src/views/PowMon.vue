dcfe39cbf784843de67e13af791f62a79e5c4dc2
ー 灰桜 ー <hhbilly99@gmail.com>
2020-01-29 23:56:13 +0800

    achieve power status getting

--- src/views/PowMon.vue
+++ src/views/PowMon.vue
@@ -12,37 +12,37 @@
               <el-col :span="4">
                 <span class="item-title">变压器A相</span>
                 <p class="item-value">{{ $root.devStatus.trasApha }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="$root.devStatus.trasApha > 80"><i class="el-icon-error" /></p>
+                <p v-if="$root.devStatus.trasApha > $root.devCfg.trtmUplm"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">变压器B相</span>
                 <p class="item-value">{{ $root.devStatus.trasBpha }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="$root.devStatus.trasBpha > 80"><i class="el-icon-error" /></p>
+                <p v-if="$root.devStatus.trasBpha > $root.devCfg.trtmUplm"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">变压器C相</span>
                 <p class="item-value">{{ $root.devStatus.trasCpha }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="$root.devStatus.trasCpha > 80"><i class="el-icon-error" /></p>
+                <p v-if="$root.devStatus.trasCpha > $root.devCfg.trtmUplm"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">母排A相</span>
                 <p class="item-value">{{ $root.devStatus.bbarAtem }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="$root.devStatus.bbarAtem > 80"><i class="el-icon-error" /></p>
+                <p v-if="$root.devStatus.bbarAtem > $root.devCfg.butmUplm"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">母排B相</span>
                 <p class="item-value">{{ $root.devStatus.bbarBtem }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="$root.devStatus.bbarBtem > 80"><i class="el-icon-error" /></p>
+                <p v-if="$root.devStatus.bbarBtem > $root.devCfg.butmUplm"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">母排C相</span>
                 <p class="item-value">{{ $root.devStatus.bbarCtem }}&nbsp;<small>&#176;C</small></p>
-                <p v-if="$root.devStatus.bbarCtem > 80"><i class="el-icon-error" /></p>
+                <p v-if="$root.devStatus.bbarCtem > $root.devCfg.butmUplm"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
             </el-row>
@@ -60,37 +60,37 @@
               <el-col :span="4">
                 <span class="item-title">ATS电源</span>
                 <p class="item-value hide">ATS</p>
-                <p v-if="!PowMonStatus.ats"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.atspAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">HP电源</span>
                 <p class="item-value hide">HP</p>
-                <p v-if="!PowMonStatus.hp"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.hppwAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">FP电源</span>
                 <p class="item-value hide">FP</p>
-                <p v-if="!PowMonStatus.fp"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.fppwAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">DTU电源</span>
                 <p class="item-value hide">DTU</p>
-                <p v-if="!PowMonStatus.dtu"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.dtupAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">非法入侵</span>
                 <p class="item-value hide">Invade</p>
-                <p v-if="!PowMonStatus.invade"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.illeAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">烟雾报警</span>
                 <p class="item-value hide">Smoke</p>
-                <p v-if="!PowMonStatus.smoke"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.smokAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
             </el-row>
@@ -137,37 +137,35 @@
               <el-col :span="4">
                 <span class="item-title">电流</span>
                 <p class="item-value">{{ $root.devStatus.tratCurr }}&nbsp;<small>A</small></p>
-                <p v-if="$root.devStatus.tratCurr > 800"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.trcuAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">电压</span>
                 <p class="item-value">{{ $root.devStatus.traoVolt }}&nbsp;<small>V</small></p>
-                <p v-if="$root.devStatus.traoVolt > 400"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.trvlAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">频率</span>
                 <p class="item-value">{{ $root.devStatus.traoFreq }}&nbsp;<small>Hz</small></p>
-                <p v-if="$root.devStatus.traoFreq > 50"><i class="el-icon-error" /></p>
-                <p v-else><i class="el-icon-success" /></p>
+                <p><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">功率因数</span>
                 <p class="item-value">{{ $root.devStatus.traoPfcc }}</p>
-                <p v-if="$root.devStatus.traoPfcc > 0.5"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.trpfAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">有功电度</span>
                 <p class="item-value">{{ $root.devStatus.trasPwhb }}&nbsp;<small>KWH</small></p>
-                <p v-if="$root.devStatus.trasPwhb > 1000"><i class="el-icon-error" /></p>
-                <p v-else><i class="el-icon-success" /></p>
+                <p><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="4">
                 <span class="item-title">剩余电流</span>
-                <p class="item-value">{{ $root.devStatus.resiCurr }}&nbsp;<small>mA</small></p>
-                <p v-if="$root.devStatus.resiCurr > 0.1"><i class="el-icon-error" /></p>
+                <p class="item-value">{{ $root.devStatus.resiCurr }}&nbsp;<small>A</small></p>
+                <p v-if="$root.devOth.resiAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
             </el-row>
@@ -185,25 +183,25 @@
               <el-col :span="6">
                 <span class="item-title">泄漏电流</span>
                 <p class="item-value">{{ $root.devStatus.leakCurr }}&nbsp;<small>mA</small></p>
-                <p v-if="$root.devStatus.leakCurr > 1"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.leakAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">雷击次数</span>
                 <p class="item-value">{{ $root.devStatus.thunStri }}&nbsp;<small>PCS</small></p>
-                <p v-if="$root.devStatus.thunStri > 10"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.thovAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">SPD&nbsp;脱扣</span>
                 <p class="item-value hide">SPD</p>
-                <p v-if="!PowMonStatus.spd"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.spdhAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
               <el-col :span="6">
                 <span class="item-title">SCB&nbsp;状态</span>
                 <p class="item-value hide">SCB</p>
-                <p v-if="!PowMonStatus.scb"><i class="el-icon-error" /></p>
+                <p v-if="$root.devOth.scbpAlam"><i class="el-icon-error" /></p>
                 <p v-else><i class="el-icon-success" /></p>
               </el-col>
             </el-row>
