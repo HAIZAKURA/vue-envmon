16e4866bfccd2babbeb3b58019883375f86510f4
ー 灰桜 ー <hhbilly99@gmail.com>
2020-01-29 23:52:40 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,44 +8,43 @@ module.exports = {
   publicPath: './',
   devServer: {
     // can be overwritten by process.env.HOST
-    host: '0.0.0.0',  
+    host: '127.0.0.1',  
     port: 8080,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/server': ''
-        }
-      },
-      '/dev': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/dev': ''
-        }
-      },
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://127.0.0.1:3000',
+    //     changeOrigin: true,
+    //     ws: true,
+    //     pathRewrite: {
+    //       '^/server': ''
+    //     }
+    //   },
+    //   '/dev': {
+    //     target: 'http://127.0.0.1:5000',
+    //     changeOrigin: true,
+    //     ws: true,
+    //     pathRewrite: {
+    //       '^/dev': ''
+    //     }
+    //   },
+    // }
   },
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
       .set('src', resolve('src'))
-      .set('common', resolve('src/common'))
       .set('components', resolve('src/components'));
   },
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
         win: {
-          icon: './public/Network.png'
+          icon: './public/timg.png'
         },
         mac: {
-          icon: './public/Network.png'
+          icon: './public/timg.png'
         },
-        productName: 'AppDemo',
+        productName: '变配电运行环境智能监控',
       }
     }
   }
